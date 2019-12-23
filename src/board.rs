66448a41899ba963928e865315a36250b1517103
Artur Laksberg <arturl@microsoft.com>
2019-12-23 14:00:29 -0800

    First playable game

--- src/board.rs
+++ src/board.rs
@@ -5,15 +5,24 @@ use crate::color::Color;
 use log::{info, warn, trace, error, set_max_level};
 
 pub struct Board {
-    board: [[Color; 8]; 8]
+    board: [[Color; 8]; 8],
 }
 
 impl Board {
     pub fn new() -> Board {
         Board {
-            board: [[Color::Empty; 8];8]
+            board: [[Color::Empty; 8];8],
         }
     }
+
+    const DIRS:  [[i32;2]; 8] = [   [-1,-1],
+                                    [-1, 0],
+                                    [-1, 1],
+                                    [ 0, 1],
+                                    [ 0,-1],
+                                    [ 1,-1],
+                                    [ 1, 0],
+                                    [ 1, 1]];
 
     pub fn print(&self) {
         println!("      A     B     C     D     E     F     G     H");
@@ -43,6 +52,63 @@ impl Board {
         self.set_at(ii,j,color);
     }
 
+    pub fn place(&mut self, i: usize, j: usize, color: Color) {
+        trace!("initial pos: {:?}", (i,j, color));
+
+        if self.get_at(i,j) != Color::Empty {
+            panic!("Cannot place to non-empty cell");
+        }
+
+        let mut total_flipped = 0;
+
+        let opposite = color.opposite();
+
+        // Scan in 8 directions:
+
+        for direction in &Board::DIRS {
+            trace!("--> {:?}", direction);
+            let mut flipped = 0;
+            let mut new_position = (i as i32, j as i32);
+            loop {
+                new_position = (new_position.0 + direction[0], new_position.1 + direction[1]);
+                if new_position.0 >= 0 && new_position.0 < 8 && new_position.1 >= 0 && new_position.1 < 8 {
+                    let bounds_checked_position = (new_position.0 as usize, new_position.1 as usize);
+                    trace!("{:?}", bounds_checked_position);
+                    let color_at_this_position = self.get_at(bounds_checked_position.0, bounds_checked_position.1);
+                    trace!("c: {:?}", color_at_this_position);
+                    if color_at_this_position == opposite {
+                        trace!("continue going in this direction");
+                        flipped += 1;
+                    } else {
+                        if color_at_this_position == color && flipped > 0 {
+                            trace!("success: can flip {} in this direction", flipped);
+                            // Go back to the beginning and flip all cells in this direction
+                            let mut flipping_position = (i as i32, j as i32);
+                            for _ in 0..flipped {
+                                flipping_position = (flipping_position.0 + direction[0], flipping_position.1 + direction[1]);
+                                self.set_at(flipping_position.0 as usize, flipping_position.1 as usize, color);
+                                total_flipped += 1;
+                            }
+                            break;
+                        }
+                        trace!("dropping this direction");
+                        break;
+                    }
+                } else {
+                    // this direction got us out of bounds, exit
+                    trace!("out of bounds for this direction");
+                    break;
+                }
+            }
+        }
+
+        if total_flipped > 0 {
+            self.set_at(i, j, color);
+        } else {
+            panic!("{:?} is not a valid position for {:?}", (i,j), color)
+        }
+    }
+
     pub fn can_place(&self, i: usize, j: usize, color: Color) -> bool {
         trace!("initial pos: {:?}", (i,j, color));
 
@@ -54,22 +120,13 @@ impl Board {
 
         // Scan in 8 directions:
 
-        let dirs = [[-1,-1],
-                    [-1, 0],
-                    [-1, 1],
-                    [ 0, 1],
-                    [ 0,-1],
-                    [ 1,-1],
-                    [ 1, 0],
-                    [ 1, 1]];
-
-        for direction in &dirs {
+        for direction in &Board::DIRS {
             trace!("--> {:?}", direction);
             let mut flipped = 0;
             let mut new_position = (i as i32, j as i32);
             loop {
                 new_position = (new_position.0 + direction[0], new_position.1 + direction[1]);
-                if new_position.0 >= 0 && new_position.0 < 8 && new_position.1 >= 0 && new_position.1 < 8{
+                if new_position.0 >= 0 && new_position.0 < 8 && new_position.1 >= 0 && new_position.1 < 8 {
                     let bounds_checked_position = (new_position.0 as usize, new_position.1 as usize);
                     trace!("{:?}", bounds_checked_position);
                     let color_at_this_position = self.get_at(bounds_checked_position.0, bounds_checked_position.1);
@@ -77,7 +134,7 @@ impl Board {
                     if color_at_this_position == opposite {
                         trace!("continue going in this direction");
                         flipped += 1;
-                    }else {
+                    } else {
                         if color_at_this_position == color && flipped > 0 {
                             trace!("success: can flip {} in this direction", flipped);
                             return true;
@@ -85,10 +142,21 @@ impl Board {
                         trace!("dropping this direction");
                         break;
                     }
-                }else {
+                } else {
                     // this direction got us out of bounds, exit
                     trace!("out of bounds for this direction");
                     break;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn has_any_moves(&self, color: Color) -> bool {
+        for i in 0..8 {
+            for j in 0..8 {
+                if self.can_place(i,j,color) {
+                    return true;
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ use crate::color::Color;
 mod board;
 use crate::board::Board;
 
+use std::io::{self, Read};
+use std::io::stdout;
+use std::io::Write;
+
 fn main() {
 
     env_logger::builder()
@@ -46,4 +50,57 @@ fn main() {
     println!("for black: {:?}", vecb);
     println!("for white: {:?}", vecw);
 
+    let mut color = Color::Black;
+
+    loop {
+        if board.has_any_moves(color) {
+            println!("Enter next move for {:?}:", color);
+        }
+        else {
+            println!("{:?} has no more moves", color);
+            color = color.opposite();
+            if board.has_any_moves(color) {
+                println!("Instead, enter next move for {:?}:", color);
+            }
+            else {
+                println!("{:?} also has no more moves. Gave over.", color);
+                return;
+            }
+        }
+        loop {
+            print!("> ");
+            stdout().flush().unwrap();
+            let mut input = String::new();
+            let stdin = io::stdin();
+            io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim().to_lowercase();
+            println!("you said '{}'", input);
+            if input == "q" {
+                return;
+            }
+            let coords = input.as_bytes();
+            if coords.len() == 2 {
+                let x = coords[0];
+                let y = coords[1];
+
+                let xi = (x - 97) as usize;
+                let yi = (y - 48) as usize;
+
+                trace!("--> {} {}", xi, yi);
+
+                if board.can_place(xi,yi,color) {
+                    board.place(xi,yi,color);
+                    board.print();
+                    break;
+                }
+
+                println!("Error: This position is not valid for {:?}. Try again.", color);
+            }
+            else {
+                println!("Error: Wrong length, must be 2 characters. Try again.");
+            }
+        }
+
+        color = color.opposite();
+    }
 }
